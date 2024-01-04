7904678e3218f7fa349b90e6c64c610b5ccbcffa
Nick Monteleone <nicholasmonteleone25@gmail.com>
2024-01-03 16:19:42 -0800

    added post and get specific item

--- itemRoutes.js
+++ itemRoutes.js
@@ -2,12 +2,57 @@
 const express = require("express");
 
 const { items } = require("./fakeDb");
+const { BadRequestError } = require("./expressError");
+const { isNumber } = require("lodash");
+
 const router = new express.Router();
 
-/** Returns list list of shopping items */
+/** Returns list of shopping items */
+
 router.get("/", function (req, res) {
   return res.json({ items });
-})
+});
 
+/** Return a single shopping item */
+
+router.get("/:name", function (req, res, next) {
+
+  // get first match for input name
+  for (const item of items) {
+    if (item.name === req.params.name) {
+      return res.json(item);
+    }
+  }
+
+  // use next to give 404 if no match for name
+  next();
+});
+
+/** Add an item to items and return it */
+
+router.post("/", function (req, res) {
+  console.log('body input for adding item:', req.body);
+
+  if (req.body === undefined) {
+    throw new BadRequestError('No input data recieved');
+  }
+
+  if (req.body.name === undefined || req.body.price === undefined) {
+    throw new BadRequestError('Name and Price are required fields.');
+  }
+
+  if (!isNumber(req.body.price)) {
+    throw new BadRequestError('Price must be a number.');
+  }
+
+  const newItem = {
+    name: req.body.name,
+    price: req.body.price,
+  };
+  items.push(newItem);
+
+  return res.json({ added: newItem });
+
+});
 
 module.exports = router;
